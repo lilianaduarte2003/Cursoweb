0cda1f7f04d6fabe509ccc2c281d0756348a56a2
lilianaduarte2003 <liliana.duarte@etif.es>
2022-05-24 17:44:42 +0200

    clase-metodo

--- 11.scope.php
+++ 11.scope.php
@@ -24,7 +24,35 @@ echo $numero; //Muestro el 7
 // }
 // mostrarNumero();
 // echo $numero; //No se puede acceder porque la variable esta dentro de la función, necesitaremos el "return"
+<?php
+// echo "<br>";
+//SCOPE = ALCANCE
+$numero = 7; //Variable global = acceso desde cualquier parte del código excepto dentro de funciones
+echo $numero; //Muestro el 7
+// function mostrarNumero(){
+// 	$numero = 13;
+// 	echo $numero;
+// echo "<br>";
+// }
+// mostrarNumero(); // Dará un error, ya que no encuentra $numero dentro de la función
+//echo $numero;//Muestro el 7
+// echo "<br>";
 
+# Para usarla dentro de la función pasar como parámentro/argumento
+// function mostrarNumero($numero){
+// 	echo $numero;
+// }
+// echo "<br>";
+// mostrarNumero(13); //Solo dentro de la funció vale 13
+// echo $numero; //Fuera sigue valiendo el 7
+// echo "<br>";
+# Lo mismo si definimos en la función y queremos utilizarla fuera
+// function mostrarNumero(){
+// 	$numero = 10;
+// }
+// mostrarNumero();
+// echo $numero; //No se puede acceder porque la variable esta dentro de la función, necesitaremos el "return"
+// echo "<br>";
 function mostrarNumero(){
 	$numero = 10;
 	return $numero;
@@ -34,11 +62,30 @@ function mostrarNumero(){
 // 	$numero = 10;
 // 	echo $numero;
 // }
+// echo "<br>";
+mostrarNumero();//10
+$numero = mostrarNumero(); //Asigno $numero =10;
+echo $numero; //10
+// echo "<br>";
+#RESUMEN:
+//1. Por parámetro para entrar la variable en la función
+//2. Con el return para sacar la variable de la función
+
+?>
+function mostrarNumero(){
+	$numero = 10;
+	return $numero;
+}
+// echo "<br>";
+// function mostrarNumero(){
+// 	$numero = 10;
+// 	echo $numero;
+// }
 
 mostrarNumero();//10
 $numero = mostrarNumero(); //Asigno $numero =10;
 echo $numero; //10
-
+// echo "<br>";
 #RESUMEN:
 //1. Por parámetro para entrar la variable en la función
 //2. Con el return para sacar la variable de la función
